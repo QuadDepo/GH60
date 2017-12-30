fa21e9f97e3e3082a7e0b7af2b6992e02b550eb7
Ryan George <ryan@qdds.nl>
2017-12-30 22:58:21 +0100

    first

---
+++ keymap.c
@@ -0,0 +1,155 @@
+#include "gh60.h"
+#include "action_layer.h"
+#include "rgblight.h"
+
+#define _DEFAULT 0
+#define _FN 1
+#define _WASD 2
+#define _SFX 3
+
+//bool esc_led_on;
+
+enum planck_keycodes {
+  DEFAULT = SAFE_RANGE
+};
+
+// Fillers to make layering more clear
+#define ______ KC_TRNS
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+/* Qwerty gui/alt/space/alt/gui
+ * ,-----------------------------------------------------------------------------------------.
+ * | Esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  | fn2 | BS  |
+ * |-----------------------------------------------------------------------------------------+
+ * | Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |   \    |
+ * |-----------------------------------------------------------------------------------------+
+ * | fn1     |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
+ * |-----------------------------------------------------------------------------------------+
+ * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | RShift    | FN  |
+ * |-----------------------------------------------------------------------------------------+
+ *         |LAlt | LGUI  |               Space                | CTRL  | RAlt |
+ *         `-----------------------------------------------------------------'
+ */
+  [_DEFAULT] = KEYMAP_HHKB( /* Basic QWERTY */
+      KC_ESC,  KC_1,    KC_2,    KC_3, KC_4, KC_5,   KC_6, KC_7, KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  LALT(KC_SPC), KC_BSPC, \
+      KC_TAB,  KC_Q,    KC_W,    KC_E, KC_R, KC_T,   KC_Y, KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  \
+      MO(_FN), KC_A,    KC_S,    KC_D, KC_F, KC_G,   KC_H, KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  \
+      OSM(MOD_LSFT), KC_Z,    KC_X,    KC_C, KC_V, KC_B,   KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(_FN), \
+      ______,  KC_LALT, KC_LGUI,             KC_SPC,             KC_RALT, KC_LCTL, ______,  ______ \
+      ),
+
+/* WASD Layer
+ * ,-----------------------------------------------------------------------------------------.
+ * |     |     |     |     |     |     |     |     |     |     |     |     |     |     |     |
+ * |-----------------------------------------------------------------------------------------+
+ * |        |     | Up  |     |     |     |     |     |     |     |     |     |     |        |
+ * |-----------------------------------------------------------------------------------------+
+ * |         | Left| Down|Right|     |     |     |     |     |     |     |     |             |
+ * |-----------------------------------------------------------------------------------------+
+ * |           |     |     |     |     |     |     |     |      |    |     |           |     |
+ * |-----------------------------------------------------------------------------------------+
+ *         |       |       |                                 |       |       |
+ *         `-----------------------------------------------------------------'
+ */
+  [_FN] = KEYMAP_HHKB( /* Layer 2 */
+      ______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_VOLD, KC_VOLU, ______,  KC_DEL, \
+      LCTL(KC_TAB),   ______,  ______,  ______, ______, ______, ______, ______, ______, ______, ______, KC_MPRV, KC_MPLY, KC_MNXT,  \
+      ______,   ______, ______,  ______,______, ______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, ______, KC_GRAVE, ______,   \
+      ______,   ______,  KC_BSPC, ______, ______, ______, ______, ______, ______, ______, S(KC_GRAVE), ______,______,  \
+      ______,   ______,  ______,                KC_ENT,                 ______, ______, ______, ______ \
+      )
+};
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+  // MACRODOWN only works in this function
+  return MACRO_NONE;
+};
+
+enum function_id {
+    RGBLED_TOGGLE,
+    RGBLED_STEP_MODE,
+    RGBLED_INCREASE_HUE,
+    RGBLED_DECREASE_HUE,
+    RGBLED_INCREASE_SAT,
+    RGBLED_DECREASE_SAT,
+    RGBLED_INCREASE_VAL,
+    RGBLED_DECREASE_VAL
+};
+
+const uint16_t PROGMEM fn_actions[] = {
+  [0]  = ACTION_FUNCTION(RGBLED_TOGGLE),
+  [1]  = ACTION_FUNCTION(RGBLED_STEP_MODE),
+  [2]  = ACTION_FUNCTION(RGBLED_INCREASE_HUE),
+  [3]  = ACTION_FUNCTION(RGBLED_DECREASE_HUE),
+  [4]  = ACTION_FUNCTION(RGBLED_INCREASE_SAT),
+  [5]  = ACTION_FUNCTION(RGBLED_DECREASE_SAT),
+  [6]  = ACTION_FUNCTION(RGBLED_INCREASE_VAL),
+  [7]  = ACTION_FUNCTION(RGBLED_DECREASE_VAL)
+};
+
+void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
+  switch (id) {
+    case RGBLED_TOGGLE:
+      if (record->event.pressed) {
+        rgblight_toggle();
+      }
+      break;
+    case RGBLED_INCREASE_HUE:
+      if (record->event.pressed) {
+        rgblight_increase_hue();
+      }
+      break;
+    case RGBLED_DECREASE_HUE:
+      if (record->event.pressed) {
+        rgblight_decrease_hue();
+      }
+      break;
+    case RGBLED_INCREASE_SAT:
+      if (record->event.pressed) {
+        rgblight_increase_sat();
+      }
+      break;
+    case RGBLED_DECREASE_SAT:
+      if (record->event.pressed) {
+        rgblight_decrease_sat();
+      }
+      break;
+    case RGBLED_INCREASE_VAL:
+      if (record->event.pressed) {
+        rgblight_increase_val();
+      }
+      break;
+    case RGBLED_DECREASE_VAL:
+      if (record->event.pressed) {
+        rgblight_decrease_val();
+      }
+      break;
+    case RGBLED_STEP_MODE:
+      if (record->event.pressed) {
+        rgblight_step();
+      }
+      break;
+  }
+}
+
+void matrix_scan_user(void) {
+    uint32_t layer = layer_state;
+
+    if (layer & (1<<1)) {
+        gh60_fn_led_on();
+    } else {
+        gh60_fn_led_off();
+    }
+
+    if (layer & (1<<2)) {
+        gh60_wasd_leds_on();
+    } else {
+        gh60_wasd_leds_off();
+    }
+
+    if (layer & (1<<3)) {
+        gh60_esc_led_on();
+    } else {
+        gh60_esc_led_off();
+    }
+};
