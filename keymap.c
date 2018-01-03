96eb9f9e7f0d212a1657ce00aaeb1d68a0c28c7e
Ryan George <george@Ryans-MacBook-Pro.local>
2018-01-03 09:43:22 +0100

    removed tapdacen

--- keymap.c
+++ keymap.c
@@ -30,8 +30,7 @@ void dance_cln_finished (qk_tap_dance_state_t *state, void *user_data) {
     register_code (KC_GRAVE);
   }
 };
-
-void dance_cln_reset (qk_tap_dance_state_t *state, void *user_data) {
+ dance_cln_reset (qk_tap_dance_state_t *state, void *user_data) {
   if (state->count == 1) {
     register_code (KC_LALT);
     register_code (KC_SPC);
@@ -62,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *         `-----------------------------------------------------------------'
  */
   [_DEFAULT] = KEYMAP_HHKB( /* Basic QWERTY */
-      KC_ESC,  KC_1,    KC_2,    KC_3, KC_4, KC_5,   KC_6, KC_7, KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  TD(I_TERM), KC_BSPC, \
+      KC_ESC,  KC_1,    KC_2,    KC_3, KC_4, KC_5,   KC_6, KC_7, KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL, KC_1, KC_BSPC, \
       KC_TAB,  KC_Q,    KC_W,    KC_E, KC_R, KC_T,   KC_Y, KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  \
       MO(_FN), KC_A,    KC_S,    KC_D, KC_F, KC_G,   KC_H, KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  \
       OSM(MOD_LSFT), KC_Z,    KC_X,    KC_C, KC_V, KC_B,   KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(_FN), \
